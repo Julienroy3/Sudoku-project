6105210d592f1ef78206d3bede61e7859cb42f50
Julien Roy <Julienroy03@gmail.com>
2017-10-16 15:52:37 +0200

    Create From and Validation

---
+++ admin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="">
+		<form action="" method="post">
+			Date du concours :<br />
+			<input type="Date" name="dateContest" min="2017-01-01"><br />
+			Heure de début :<br />
+			<input type="Time" name="timeStart"><br />
+			Heure de fin :<br />
+			<input type="Time" name="timeEnd"><br />
+			<br />
+			<button type="button" class="js-generate-board-btn--easy">Easy</button>
+            <button type="button" class="js-generate-board-btn--medium">Medium</button>
+            <button type="button" class="js-generate-board-btn--hard">Hard</button>
+            <button type="button" class="js-generate-board-btn--very-hard">Very Hard</button>
+			<hr />
+
+			<input type="submit" name="submit" value="Go">
+
+		</form>
+	</div>
+	
+</body>
+</html>
