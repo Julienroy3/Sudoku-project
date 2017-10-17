b67d5d42ca76b887830a7e89a52a0c6d41064ea1
Elliezar Rayray <elliezar.rayray@gmail.com>
2017-10-17 11:50:26 +0200

    modified database contest and performance

--- base.sql
+++ base.sql
@@ -2,8 +2,8 @@
 -- version 4.7.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:8889
--- Généré le :  lun. 16 oct. 2017 à 17:07
+-- Hôte : localhost
+-- Généré le :  mar. 17 oct. 2017 à 11:43
 -- Version du serveur :  5.6.35
 -- Version de PHP :  7.1.6
 
@@ -22,7 +22,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `Concours` (
   `IdConcours` int(11) NOT NULL,
-  `IdGrille` int(11) DEFAULT NULL
+  `GrilleConcours` varchar(200) DEFAULT NULL,
+  `DateConcours` date DEFAULT NULL,
+  `HeureDebut` time DEFAULT NULL,
+  `HeureFin` time DEFAULT NULL,
+  `GrilleSolution` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -32,8 +36,8 @@ CREATE TABLE `Concours` (
 --
 
 CREATE TABLE `Participe` (
-  `Temps` time DEFAULT NULL,
-  `Classement` int(11) DEFAULT NULL,
+  `Temps` time NOT NULL,
+  `Classement` int(11) NOT NULL,
   `IdUser` int(11) NOT NULL,
   `IdConcours` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -46,9 +50,10 @@ CREATE TABLE `Participe` (
 
 CREATE TABLE `Performances` (
   `IdGrille` int(11) NOT NULL,
-  `DateResolution` date DEFAULT NULL,
-  `TempsResolu` int(11) DEFAULT NULL,
-  `Niveau` varchar(25) DEFAULT NULL
+  `DateResolu` date NOT NULL,
+  `TempsResolu` time NOT NULL,
+  `Niveau` varchar(25) NOT NULL,
+  `IdUser` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -62,20 +67,10 @@ CREATE TABLE `Utilisateur` (
   `username` varchar(25) NOT NULL,
   `password` text NOT NULL,
   `email` varchar(25) NOT NULL,
-  `icon` varchar(200) NOT NULL DEFAULT 'default.png',
+  `icon` varchar(200) NOT NULL,
   `date_sign` date NOT NULL,
-  `admin` int(11) NOT NULL DEFAULT '0',
-  `IdGrille` int(11) DEFAULT NULL
+  `admin` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `Utilisateur`
---
-
-INSERT INTO `Utilisateur` (`IdUser`, `username`, `password`, `email`, `icon`, `date_sign`, `admin`, `IdGrille`) VALUES
-(4, 'za', '959848ca10cc8a60da818ac11523dc63', 'myriszadip@gmail.com', 'default.png', '2017-10-16', 0, NULL),
-(5, 'tif', '423c3fba9840072ab5db43ced5d96f83', 'tif@tif.fr', 'default.png', '2017-10-16', 0, NULL),
-(6, 'sq', '4bc92a7aeb9478e6bf3f989025232b22', 'myriszadip@gmail.com', 'default.png', '2017-10-16', 0, NULL);
 
 --
 -- Index pour les tables déchargées
@@ -98,15 +93,15 @@ ALTER TABLE `Participe`
 -- Index pour la table `Performances`
 --
 ALTER TABLE `Performances`
-  ADD PRIMARY KEY (`IdGrille`);
+  ADD PRIMARY KEY (`IdGrille`),
+  ADD KEY `FK_Performances_IdUser` (`IdUser`);
 
 --
 -- Index pour la table `Utilisateur`
 --
 ALTER TABLE `Utilisateur`
   ADD PRIMARY KEY (`IdUser`),
-  ADD UNIQUE KEY `Username` (`username`),
-  ADD KEY `FK_Utilisateur_IdGrille` (`IdGrille`);
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -126,7 +121,7 @@ ALTER TABLE `Performances`
 -- AUTO_INCREMENT pour la table `Utilisateur`
 --
 ALTER TABLE `Utilisateur`
-  MODIFY `IdUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `IdUser` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Contraintes pour les tables déchargées
 --
@@ -139,7 +134,7 @@ ALTER TABLE `Participe`
   ADD CONSTRAINT `FK_Participe_IdUser` FOREIGN KEY (`IdUser`) REFERENCES `Utilisateur` (`IdUser`);
 
 --
--- Contraintes pour la table `Utilisateur`
+-- Contraintes pour la table `Performances`
 --
-ALTER TABLE `Utilisateur`
-  ADD CONSTRAINT `FK_Utilisateur_IdGrille` FOREIGN KEY (`IdGrille`) REFERENCES `Performances` (`IdGrille`);
+ALTER TABLE `Performances`
+  ADD CONSTRAINT `FK_Performances_IdUser` FOREIGN KEY (`IdUser`) REFERENCES `Utilisateur` (`IdUser`);
