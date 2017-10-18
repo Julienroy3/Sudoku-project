9738ed36a4f7bdffd962dbc7ac952599a006ac93
Myris <myriszadip@gmail.com>
2017-10-18 14:40:48 +0200

    display last winners

--- contest.php
+++ contest.php
@@ -1,50 +1,59 @@
-<!DOCTYPE html>
-<html>
-<?php include("connect.php");
+
+<?php 
+    include("connect.php");
       session_start(); 
       include 'header.php';
       include 'menu.php';
-    
-    $req = $bdd->prepare("SELECT Participe.Classement, Participe.IdUser, Participe.IdConcours, Utilisateur.IdUser, Utilisateur.username, Concours.IdConcours, Concours.DateConcours, @current := (SELECT MAX(Concours.IdConcours) FROM Concours) FROM Participe, Utilisateur, Concours WHERE Participe.IdUser = Utilisateur.IdUser AND Participe.IdConcours = Concours.IdConcours AND Classement = '1'");
-    //$req = $bdd->prepare("SELECT Participe.Classement, Participe.IdUser, Participe.IdConcours, Utilisateur.IdUser, Utilisateur.username, Concours.IdConcours, max(Concours.IdConcours) as current, Concours.DateConcours FROM Participe, Utilisateur, Concours WHERE Participe.IdUser = Utilisateur.IdUser  AND Participe.IdConcours = Concours.IdConcours AND Classement = '1'");
-    //$req = $bdd->prepare("SELECT max(IdConcours) FROM Concours WHERE 1");
-    //$req->bindParam(":IdUser", $_SESSION["IdUser"], PDO::PARAM_INT);
-    $req->execute();
-    
-    while ($donnees = $req->fetch()){
 ?>
 <body>
-	<div class="container">
-        <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6 center">
-                <h1>Concours</h1>
-                <?php if(isset($_SESSION['IdUser'])) { ?>
-                <a class="btn_contest" href="contest_join.php?idconcours=<?php echo $donnees["@current := (SELECT MAX(Concours.IdConcours) FROM Concours)"];?>">Je participe</a>
-                <?php } else { ?>
-                <a class="btn_contest" href="sign_up.php">Je participe</a>
+    <?php
+    
+    $req2 = $bdd->prepare("SELECT MAX(Concours.IdConcours) as maxConcours FROM Concours");
+    $req2->execute();
+    
+    while ($donnees = $req2->fetch()){
+        ?>
+    
+    <div class="container">
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6 center">
+                    <h1>Concours</h1>
+                    <?php if(isset($_SESSION['IdUser'])) { ?>
+                    <a class="btn_contest" href="contest_join.php?idconcours=<?php echo $donnees["maxConcours"];?>">Je participe</a>
+                    <?php } else { ?>
+                    <a class="btn_contest" href="sign_up.php">Je participe</a>
                 <?php } ?>
 		  </div>
         </div>
-        
-        <div class="row winner-array">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
-                <div class="col-xs-6 col-md-6 winner-tab">Date</div>
-                <div class="col-xs-6 col-md-6 winner-tab">Vainqueur</div>
+    
+    <?php
+    }
+    
+    $req2->closeCursor();
+    
+    $req3 = $bdd->prepare("SELECT Participe.Classement, Participe.IdUser, Participe.IdConcours, Utilisateur.IdUser, Utilisateur.username, Concours.IdConcours, Concours.DateConcours FROM Participe, Utilisateur, Concours WHERE Participe.IdUser = Utilisateur.IdUser AND Participe.IdConcours = Concours.IdConcours AND Classement = '1'");
+    $req3->execute();
+        ?>
+    
+    <div class="row winner-array">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <div class="col-xs-6 col-md-6 winner-tab">Date</div>
+            <div class="col-xs-6 col-md-6 winner-tab">Vainqueur</div>
                 
-                <?php
-                    
-                ?>
-                <div class="col-xs-6 col-md-6"><?php echo $donnees["DateConcours"]; ?></div>
-                <div class="col-xs-6 col-md-6"><?php echo $donnees["username"]; ?></div>
-                <?php
-                    }
-                ?>
+            <?php
+                
+            while($donnees = $req3->fetch()){ ?>
+
+            <div class="col-xs-6 col-md-6"><?php echo $donnees["DateConcours"]; ?></div>
+            <div class="col-xs-6 col-md-6"><?php echo $donnees["username"]; ?></div>
+            <?php
+                }
+            $req3->closeCursor();
+            ?>
             </div>
             <div class="col-md-3"></div>
         </div>
-        
-	</div>
+    </div>
 </body>
-</html>
