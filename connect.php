f1720b62adcc1e714dde2920128c8ecf92a09c96
Julien Roy <Julienroy03@gmail.com>
2017-10-19 19:04:21 +0200

    Why not?

--- connect.php
+++ connect.php
@@ -1,7 +1,11 @@
 <?php
 
 try{
+<<<<<<< Updated upstream
     $bdd = new PDO('mysql:host=localhost;dbname=sudoku;charset=utf8', 'root', 'root');
+=======
+    $bdd = new PDO('mysql:host=localhost:8181;dbname=sudoku;charset=utf8', 'root', 'root');
+>>>>>>> Stashed changes
 }catch (Exception $e){
     die('Erreur : ' . $e->getMessage());
 }
