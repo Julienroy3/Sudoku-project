e53c6cfb5334cbfe931ba2b9c0ba1a247671c998
tiffanyfl <tiffanyf.levacher@gmail.com>
2017-10-20 09:22:25 +0200

    Delete footer

--- footer.php
+++ footer.php
@@ -1,12 +1,14 @@
 </div>
         </div>
 
+<!--
 <footer class="navbar-fixed-bottom">
     <div class="col-md-12">
         <div class="text-center">Fait avec <3</div>
         <div class="text-center">2017</div>
     </div>
 </footer>
+-->
         
         
     </body>
